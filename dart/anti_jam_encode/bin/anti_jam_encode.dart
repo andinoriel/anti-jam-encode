89378c70b8f5d8bd37d5e2fad1f79d045b48dbeb
IceArrow256 <icearrow256@gmail.com>
2021-03-21 14:34:42 +0200

    add new printing & increase penalty while method best = last

--- dart/anti_jam_encode/bin/anti_jam_encode.dart
+++ dart/anti_jam_encode/bin/anti_jam_encode.dart
@@ -1,38 +1,100 @@
-import 'package:anti_jam_encode/anti_jam_encode.dart' as anti_jam_encode;
+import 'dart:math';
+
+import 'package:anti_jam_encode/anti_jam_encode.dart';
 import 'package:intl/intl.dart';
 
 void main(List<String> arguments) {
-  print('# Capacity of the communication channel where k(signal types): 9 and '
-      't(time): 0.23');
-  print('\tC: ${anti_jam_encode.calcCapacity(k: 9, t: 0.23)}\n');
-  print('# Calculation of characteristics of communication channel with '
-      'interference');
   var numberFormat = NumberFormat.simpleCurrency(locale: 'uk');
   var numberFormatDecimal = NumberFormat.decimalPattern();
-  for (var n in <int>[1, 3, 5, -1]) {
-    var data = <String, double>{};
-    if (n == -1) {
-      data = anti_jam_encode.calcCharsCommunChannel(
-          i: 3.4, c1: 0.02, c0: 2, parityBit: 4);
-      print('## With parity bit');
-    } else {
-      data =
-          anti_jam_encode.calcCharsCommunChannel(n: n, i: 3.4, c1: 0.02, c0: 2);
-      print('## Number of reps is $n');
-    }
-
-    print(
-        '\tTotal transmitted (vu): ${numberFormatDecimal.format(data['vu'])} bits');
-    print(
-        '\tCorrect transmitted (v1): ${numberFormatDecimal.format(data['v1'])} bits');
-    print(
-        '\tInorrect transmitted (v0): ${numberFormatDecimal.format(data['v0'])} bits');
-    if (n == -1) {
-      print(
-          '\tSkipped inorrect transmitted  (v2): ${numberFormatDecimal.format(data['v2'])} bits');
-    }
-    print('\tIncome (s1): ${numberFormat.format(data['s1'])}');
-    print('\tPenalty (s0): ${numberFormat.format(data['s0'])}');
-    print('\tNet income (sΔ): ${numberFormat.format(data['sdelta'])}');
+  var k = 9;
+  var t = 0.23;
+  var bits = pow(10, 6);
+  var q = pow(10, -5);
+  var p = 1 - q;
+  var i = 3.0;
+  var c0 = 3.4;
+  var c1 = 0.04;
+  print(
+      'Capacity of the communication channel: ${numberFormatDecimal.format(calcCapacity(k: 9, t: 0.23))}, where k(signal types): $k and t(time): $t:\n');
+  var data = <Map<String, double>>[];
+  data.add(calcCharsCommunChannel(
+      bits: bits, p: p, q: q, i: i, c0: c0, c1: c1, n: 1));
+  data.add(calcCharsCommunChannel(
+      bits: bits, p: p, q: q, i: i, c0: c0, c1: c1, n: 3));
+  data.add(calcCharsCommunChannel(
+      bits: bits, p: p, q: q, i: i, c0: c0, c1: c1, n: 5));
+  data.add(calcCharsCommunChannelWithParityBit(
+      bits: bits, p: p, q: q, i: i, c0: c0, c1: c1, parityBit: 4));
+  print_data(data);
+  print('Penalty: ${c0.toStringAsFixed(2)}');
+  while (data[0]['sdelta'] > data[3]['sdelta']) {
+    data[0] = calcCharsCommunChannel(
+        bits: bits, p: p, q: q, i: i, c0: c0, c1: c1, n: 1);
+    data[1] = calcCharsCommunChannel(
+        bits: bits, p: p, q: q, i: i, c0: c0, c1: c1, n: 3);
+    data[2] = calcCharsCommunChannel(
+        bits: bits, p: p, q: q, i: i, c0: c0, c1: c1, n: 5);
+    data[3] = calcCharsCommunChannelWithParityBit(
+        bits: bits, p: p, q: q, i: i, c0: c0, c1: c1, parityBit: 4);
+    c0 += 0.001;
   }
+  print_data(data);
+  print('Penalty: ${c0.toStringAsFixed(2)}');
+}
+
+void print_data(
+  List<Map<String, double>> data,
+) {
+  var length = 19;
+  var nameLength = 35;
+  var string = ''.padLeft(nameLength);
+  for (var element in [
+    'With a 1 repetition',
+    'With 2 repetitions',
+    'With 3 repetitions',
+    'With parity bit'
+  ]) {
+    string += ' ${element.padLeft(length)} |';
+  }
+  print(string);
+  string = 'Total Transmitted (vu) |'.padLeft(nameLength);
+  for (var element in data) {
+    string += ' ${element['vu'].toStringAsFixed(2).padLeft(length)} |';
+  }
+  print(string);
+  string = 'Correct Transmitted (v1) |'.padLeft(nameLength);
+  for (var element in data) {
+    string += ' ${element['v1'].toStringAsFixed(2).padLeft(length)} |';
+  }
+  print(string);
+  string = 'Inorrect Transmitted (v0) |'.padLeft(nameLength);
+  for (var element in data) {
+    string += ' ${element['v0'].toStringAsFixed(2).padLeft(length)} |';
+  }
+  print(string);
+  string = 'Skipped Inorrect Transmitted (v2) |'.padLeft(nameLength);
+  for (var element in data) {
+    string += ' ${element['v2'].toStringAsFixed(2).padLeft(length)} |';
+  }
+  print(string);
+  string = 'Income (s1) |'.padLeft(nameLength);
+  for (var element in data) {
+    string += ' ${element['s1'].toStringAsFixed(2).padLeft(length)} |';
+  }
+  print(string);
+  string = 'Penalty (s0) |'.padLeft(nameLength);
+  for (var element in data) {
+    string += ' ${element['s0'].toStringAsFixed(2).padLeft(length)} |';
+  }
+  print(string);
+  string = 'Net Income (sΔ) |'.padLeft(nameLength);
+  for (var element in data) {
+    string += ' ${element['sdelta'].toStringAsFixed(2).padLeft(length)} |';
+  }
+  print(string);
+  var max_income = 0.0;
+  for (var element in data) {
+    max_income = max(max_income, element['sdelta']);
+  }
+  print('Max Net Income: ${max_income.toStringAsFixed(2)}');
 }
